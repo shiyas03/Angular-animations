740d819b5fa8b842b9f29b951c2314547c85b7a4
shiyas03 <shiyastofficial@gmail.com>
2023-11-14 17:55:32 +0530

    ng-deep selector used in course component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseComponent } from './components/course/course.component';
+import { AuthorComponent } from './components/author/author.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseComponent
+    CourseComponent,
+    AuthorComponent
   ],
   imports: [
     BrowserModule,
