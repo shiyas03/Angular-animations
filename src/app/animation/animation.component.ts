5da5a5b300194528b8e7b56e1c8f27494b27cffb
shiyas03 <shiyastofficial@gmail.com>
2023-11-20 22:40:42 +0530

    multistep transition at animation component

--- src/app/animation/animation.component.ts
+++ src/app/animation/animation.component.ts
@@ -5,26 +5,48 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-animation',
   templateUrl: './animation.component.html',
   styleUrls: ['./animation.component.css'],
-  animations: [trigger('clickedState', [
-    state('default', style({
+  animations: [
+    trigger('clickedState', [
+      state('default', style({
         width: '100px',
         height: '100px',
         backgroundColor: 'orange'
       })),
-    state('clicked', style({
-      width: '200px',
-      height: '200px',
-      backgroundColor: 'blue'
-    })),
-    transition('default => clicked',animate('1s 200ms ease-out'))
-  ])]
+      state('clicked', style({
+        width: '200px',
+        height: '200px',
+        backgroundColor: 'blue'
+      })),
+      transition('default => clicked', animate('1s 200ms ease-out'))
+    ]),
+    trigger('numberState', [
+      state('unselected', style({
+        border: '1px solid black',
+        padding: '5px'
+      })),
+      state('selected', style({
+        border: '2px solid blue',
+        padding: '4px',
+        backgroundColor: 'lightblue'
+      })),
+      transition('unselected => selected', [style({
+        border: '2px solid black',
+        padding: '4px'
+      }), animate(300)])
+    ])
+  ]
 })
 export class AnimationComponent implements OnInit {
   clickedInfo: string = 'default'
 
+  selectedNumber!: number
+
+  onNumberSelected(event: Event) {
+    this.selectedNumber = +(<HTMLInputElement>event.target).value
+  }
   ngOnInit(): void {
-      setTimeout(()=>{
-        this.clickedInfo = 'clicked'
-      },3000)
+    setTimeout(() => {
+      this.clickedInfo = 'clicked'
+    }, 3000)
   }
 }
