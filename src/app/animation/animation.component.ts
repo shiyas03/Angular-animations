6d0109e1f4d18be25f32df26cbcef2f831827ae3
shiyas03 <shiyastofficial@gmail.com>
2023-11-20 22:00:02 +0530

    transition animation used in animation component

--- src/app/animation/animation.component.ts
+++ src/app/animation/animation.component.ts
@@ -1,12 +1,30 @@
-import { trigger } from '@angular/animations';
-import { Component } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-animation',
   templateUrl: './animation.component.html',
   styleUrls: ['./animation.component.css'],
-  animations:[trigger('clickedState',[])]
+  animations: [trigger('clickedState', [
+    state('default', style({
+        width: '100px',
+        height: '100px',
+        backgroundColor: 'orange'
+      })),
+    state('clicked', style({
+      width: '200px',
+      height: '200px',
+      backgroundColor: 'blue'
+    })),
+    transition('default => clicked',animate('1s 200ms ease-out'))
+  ])]
 })
-export class AnimationComponent {
+export class AnimationComponent implements OnInit {
+  clickedInfo: string = 'default'
 
+  ngOnInit(): void {
+      setTimeout(()=>{
+        this.clickedInfo = 'clicked'
+      },3000)
+  }
 }
