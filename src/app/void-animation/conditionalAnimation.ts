acd5b6273f64274a9bab6a7df3238df0d14883d2
shiyas03 <shiyastofficial@gmail.com>
2023-12-01 22:25:55 +0530

    dynamic dimensional properties & wildcard used

--- src/app/void-animation/conditionalAnimation.ts
+++ src/app/void-animation/conditionalAnimation.ts
@@ -1,8 +1,13 @@
 import { animate, state, style, transition, trigger } from "@angular/animations";
 
 export const conditionAnimation = trigger('conditionalTrigger', [
-    state('shown', style({})),
-
     transition('void => shown', [style({ opacity: 0 }), animate(300, style({ opacity: 1 }))]),
     transition('shown => void', [animate(300, style({ opacity: 0 }))])
+])
+
+export const shrinkAnimation = trigger('shrinkAnimation', [
+    transition('* => *', [
+        animate(400, style({ width: 0 })),
+        animate(400, style({ width: '*' }))
+    ])
 ])
--- src/app/void-animation/void-animation.component.ts
+++ src/app/void-animation/void-animation.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
-import { conditionAnimation } from './conditionalAnimation';
+import { conditionAnimation, shrinkAnimation } from './conditionalAnimation';
 
 @Component({
   selector: 'app-void-animation',
   templateUrl: './void-animation.component.html',
   styleUrls: ['./void-animation.component.css'],
-  animations: [conditionAnimation]
+  animations: [conditionAnimation, shrinkAnimation]
 })
 export class VoidAnimationComponent {
   toggleContent: boolean = false
+  animate: boolean = false
+  width: number = 400
 
   showContent() {
     this.toggleContent = !this.toggleContent
