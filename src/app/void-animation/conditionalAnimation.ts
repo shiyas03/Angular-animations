8a23b1e618f3d7d1910f19068c32d7341c0be00a
shiyas03 <shiyastofficial@gmail.com>
2023-12-01 22:41:05 +0530

    group method for parellel animatin used

--- src/app/void-animation/conditionalAnimation.ts
+++ src/app/void-animation/conditionalAnimation.ts
@@ -1,8 +1,8 @@
-import { animate, state, style, transition, trigger } from "@angular/animations";
+import { animate, group, state, style, transition, trigger } from "@angular/animations";
 
 export const conditionAnimation = trigger('conditionalTrigger', [
-    transition('void => shown', [style({ opacity: 0 }), animate(300, style({ opacity: 1 }))]),
-    transition('shown => void', [animate(300, style({ opacity: 0 }))])
+    transition(':enter', [style({ opacity: 0 }), animate(300, style({ opacity: 1 }))]),
+    transition(':leave', [animate(300, style({ opacity: 0 }))])
 ])
 
 export const shrinkAnimation = trigger('shrinkAnimation', [
@@ -10,4 +10,16 @@ export const shrinkAnimation = trigger('shrinkAnimation', [
         animate(400, style({ width: 0 })),
         animate(400, style({ width: '*' }))
     ])
+])
+
+export const listAnimation = trigger('listAnimation', [
+    transition(':enter', [
+        style({ opacity: 1, backgroundColor: 'white' }),
+        group([
+            animate(1000, style({ opacity: 1 })),
+            animate(1500, style({ backgroundColor: 'red' }))
+        ]),
+        animate(1000)
+    ]),
+    transition(':leave', [animate(300, style({ opacity: 0 }))])
 ])
--- src/app/void-animation/void-animation.component.ts
+++ src/app/void-animation/void-animation.component.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
-import { conditionAnimation, shrinkAnimation } from './conditionalAnimation';
+import { conditionAnimation, listAnimation, shrinkAnimation } from './conditionalAnimation';
 
 @Component({
   selector: 'app-void-animation',
   templateUrl: './void-animation.component.html',
   styleUrls: ['./void-animation.component.css'],
-  animations: [conditionAnimation, shrinkAnimation]
+  animations: [conditionAnimation, shrinkAnimation, listAnimation]
 })
 export class VoidAnimationComponent {
   toggleContent: boolean = false
   animate: boolean = false
   width: number = 400
+  listData: number[] = []
 
   showContent() {
     this.toggleContent = !this.toggleContent
+  }
+
+  onAddElement() {
+    this.listData.push(Math.random())
   }
 }
