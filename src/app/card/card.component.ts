1cb8512b9bfb6d0d1d651d1206d4e57fd8ee1e6a
shiyas03 <shiyastofficial@gmail.com>
2023-12-10 11:25:45 +0530

    query animation used in card component

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { animate, style, transition, trigger } from '@angular/animations';
+import { animate, group, query, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,21 +8,55 @@ import { Component } from '@angular/core';
   animations: [
     trigger('cardAnimationTrigger', [
       transition(":enter", [
-        style({
-          opacity: 0,
-          transform: 'translateX(100%)'
-        }),
-        animate(300)
+        group([
+          query('.card-header,.card-footer', [
+            style({
+              opacity: 0
+            })
+          ]),
+          style({
+            opacity: 0,
+            transform: 'translateX(100%)'
+          }),
+          animate(500),
+          query('.card-header', [
+            style({
+              transform: 'translateY(-100%)'
+            }),
+            animate(500)
+          ]),
+          query('.card-footer', [
+            style({
+              transform: 'translateY(100%)'
+            }),
+            animate(500)
+          ])
+        ])
       ]),
       transition(":leave", [
         animate(300, style({
           opacity: 0,
           transform: 'translateX(-100%)'
         }))
+      ]),
+      transition('* => *', [
+        query(':enter', [
+          style({
+            transform: 'scale(1)'
+          }),
+          animate(300, style({
+            transform: 'scale(1.1)'
+          })),
+          animate(300)
+        ],
+          {
+            optional: true
+          })
       ])
     ])
   ]
 })
 export class CardComponent {
   showCard: boolean = true
+  showParagraph: boolean = true
 }
